67df12d70a1ad0686c80142c74b3e8d162b545f4
Robert Cvitkovic <robert.cvitkovic@xlab.si>
2017-02-22 15:25:28 +0100

    Add bluetooth sensors location visualization
    The plot_bt crates a visualization of the location of bluethooth
    sensors in Ljubljana.

---
+++ plot_bt.py
@@ -0,0 +1,51 @@
+import requests
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+from cartopy.io.img_tiles import OSM
+
+s = requests.Session()
+s.verify = 'DigiCertSHA2SecureServerCA.crt'
+s.auth = ('username', 'password')
+response = s.get('https://datacloud-timon.xlab.si/data_access/bt_sensors/sensors/')
+
+data = {'data':[]}
+if response.status_code == 200:
+    data = response.json()
+else:
+    exit()
+
+name = []
+lng = []
+lat = []
+
+#not_lj = ['BTR0219', 'BTR0218', 'BTR0217', 'BTR0213']
+not_lj = []
+
+
+for point in data['data']:
+    if point['btId'] not in not_lj:
+        name.append(point['btId'])
+        lng.append(point['loc']['lng'])
+        lat.append(point['loc']['lat'])
+
+
+imagery = OSM()
+plt.figure(figsize=(15, 15), dpi=600)
+ax = plt.axes(projection=imagery.crs, )
+ax.set_extent((min(lng)-0.02, max(lng)+0.02, min(lat)-0.01, max(lat)+0.01))
+
+ax.add_image(imagery, 12)
+
+plt.plot(lng, lat, 'bo', transform=ccrs.Geodetic(),
+         markersize=5)
+
+for i in range(len(lng)):
+    plt.text(lng[i]+0.001, lat[i]+0.0005, name[i],
+         horizontalalignment='left', fontsize=10,
+         transform=ccrs.Geodetic())
+
+# plt.title('BT v Ljubljani')
+# plt.savefig("bt_lj.png")
+
+plt.title('Vse BT')
+plt.savefig("vse_bt.png")
