90da824d086633a46b93d3d3e8fcb81ff726a4f6
Robert Cvitkovic <robert.cvitkovic@xlab.si>
2017-02-27 15:14:16 +0100

    Add request timeout to bluethooth collector
    Because datacloud-timon site is not fully stable we need to add a
    timeout and a reconnecting circle to the request.

--- bt/datacloud_bt.py
+++ bt/datacloud_bt.py
@@ -1,12 +1,17 @@
 import requests
 
-response = requests.get('https://datacloud-timon.xlab.si/data_access/bt_sensors/velocities_avgs_last/', auth=('username', 'password'), verify='DigiCertSHA2SecureServerCA.crt')
-
-data = {'data':[]}
-if response.status_code == 200:
-    data = response.json()
-
-print(data)
+n=100
+while n > 0:
+    try:
+        response = requests.get('https://datacloud-timon.xlab.si/data_access/bt_sensors/velocities_avgs_last/',
+                                auth=('username', 'password'), verify='DigiCertSHA2SecureServerCA.crt', timeout=0.1)
+        if response.status_code == 200:
+            data = response.json()
+            break
+    except:
+        n -= 1
+else:
+    exit()
 
 not_lj = ['BTR0219', 'BTR0218', 'BTR0217', 'BTR0213']
 
--- bt/kafka_bt.py
+++ bt/kafka_bt.py
@@ -9,15 +9,23 @@ client.add_topic('bt_json')
 
 producer = KafkaProducer(bootstrap_servers=['192.168.0.62:9092'], value_serializer=lambda m: json.dumps(m).encode('ascii'))
 
-response = requests.get('https://datacloud-timon.xlab.si/data_access/bt_sensors/velocities_avgs_last/', auth=('username', 'password'), verify='DigiCertSHA2SecureServerCA.crt')
+n=100
+while n > 0:
+    try:
+        response = requests.get('https://datacloud-timon.xlab.si/data_access/bt_sensors/velocities_avgs_last/',
+                                auth=('username', 'password'), verify='DigiCertSHA2SecureServerCA.crt', timeout=0.1)
+        if response.status_code == 200:
+            data = response.json()
+            break
+    except:
+        n -= 1
+else:
+    exit()
 
 not_lj = ['BTR0219', 'BTR0218', 'BTR0217', 'BTR0213']
 
-data = {'data':[]}
-if response is not None and response.status_code == 200:
-    data = response.json()
-    for dist in data['data']:
-        if dist['toBtId'] not in not_lj and dist['fromBtId'] not in not_lj:
-            producer.send('bt_json', dist)
+for dist in data['data']:
+    if dist['toBtId'] not in not_lj and dist['fromBtId'] not in not_lj:
+        producer.send('bt_json', dist)
 
 
