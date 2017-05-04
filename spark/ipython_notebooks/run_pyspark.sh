819dd28d5d844e2ff8fc2a7ac69b74ecef0ab3d4
Robert Cvitkovic <robert.cvitkovic@xlab.si>
2017-05-04 15:05:00 +0200

    Delete ipython notebooks
    Those notebooks were for testing and learning purposes only and so do
    not add any functionality to the project.
    
    Change-Id: Id174e843a00214067cbf124f9a2aa6a132aeb787

--- spark/ipython_notebooks/run_pyspark.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PYSPARK_DRIVER_PYTHON=ipython2 \
-PYSPARK_DRIVER_PYTHON_OPTS="notebook --ip=* --port=12345 --no-browser" \
-$SPARK_HOME/bin/pyspark \
---packages com.datastax.spark:spark-cassandra-connector_2.11:2.0.0-M3 \
---conf spark.cassandra.connection.host=10.10.43.76 \
---master local[6] --executor-memory 8GB
